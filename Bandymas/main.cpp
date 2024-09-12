7b571904f7e1ea0165b1a5da3a86cb0f3b7d1ea1
FlyingDevilMan <flyingdevilman@gmail.com>
2024-09-12 15:18:43 +0300

    pabaigta 2 uzduotis

--- Bandymas/main.cpp
+++ Bandymas/main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
-
+#include <string>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    //pirma uzduotele
+    cout<<"--------1 uzuotis-------"<<endl;
+
+    string vardas, pavarde, grupe, kursas, studijuprg;
+    int amzius;
+
+    vardas = "Nojus";
+    pavarde = "Rudys";
+    grupe = "PI24";
+    kursas = "1 kursas";
+    studijuprg = "Programu sistemos";
+    amzius = 20;
+
+    cout << "Studento vardas: " <<vardas<<endl;
+    cout << "Studento pavarde: " <<pavarde<<endl;
+    cout << "Studento amzius: " <<amzius<<endl;
+    cout << "Studento grupe: " <<grupe<<endl;
+    cout << "Studento kursas: " <<kursas<<endl;
+    cout << "Studento studiju programa: " <<studijuprg<<endl;
+
+    //antra uzduotis
+    // farena - futbolo arena
+    // i_metai - ikurimo metai
+    // vskacius - vietu skaicius
+    cout<<"--------2 uzuotis-------"<<endl;
+    string pavadinimas, savininkas, farena;
+    int i_metai, vskaicius;
+
+    pavadinimas = "Vilniaus zalgiris";
+    savininkas = "Lietuvos futbolo federacija";
+    farena = "Vilniaus FFL arena";
+    i_metai = 1947;
+    vskaicius = 5067;
+
+    cout << "Komandos pavadinimas: " <<pavadinimas<<endl;
+    cout << "Klubo savininas: " <<savininkas<<endl;
+    cout << "Futbolo arena: " <<farena<<endl;
+    cout << "ikurimo metai: " <<i_metai<<endl;
+    cout << "vietu sakicius: " <<vskaicius<<endl;
+
 }
