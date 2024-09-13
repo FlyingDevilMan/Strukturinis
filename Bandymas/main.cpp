5f54928479ca83136d7fcf0b0a4eab7853fe416d
FlyingDevilMan <flyingdevilman@gmail.com>
2024-09-13 15:14:10 +0300

    pabaigta 5 uzduotis

--- Bandymas/main.cpp
+++ Bandymas/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
+#include <iomanip> //biblioteka skirta tikslumui nustatyti
 using namespace std;
 int main() {
-    //pirma uzduotele
     cout<<"--------1 uzuotis-------"<<endl;
-
+    //studijuprg - studiju programa
     string vardas, pavarde, grupe, kursas, studijuprg;
     int amzius;
 
@@ -22,11 +22,11 @@ int main() {
     cout << "Studento kursas: " <<kursas<<endl;
     cout << "Studento studiju programa: " <<studijuprg<<endl;
 
-    //antra uzduotis
+
+    cout<<"--------2 uzuotis-------"<<endl;
     // farena - futbolo arena
     // i_metai - ikurimo metai
     // vskacius - vietu skaicius
-    cout<<"--------2 uzuotis-------"<<endl;
     string pavadinimas, savininkas, farena;
     int i_metai, vskaicius;
 
@@ -42,4 +42,48 @@ int main() {
     cout << "ikurimo metai: " <<i_metai<<endl;
     cout << "vietu sakicius: " <<vskaicius<<endl;
 
+    cout<<"--------3 uzuotis-------"<<endl;
+    //pmetai - pagaminimo metai
+    string marke, modelis, spalva, litrazas;
+    int pmetai;
+
+    marke = "Volkswagen";
+    modelis = "golf plus";
+    spalva = "sviesiai ruda";
+    pmetai = 2012;
+    litrazas = "1.4";
+
+    cout << "Automobilis " <<marke <<" "<<modelis<<", kurio spalva - "
+    <<spalva<<". Masiniukas buvo pagamintas "<<pmetai<<"-ais metais, sis grazuolis turi "
+    <<litrazas<<"l turio varikliuka." <<endl;
+
+
+    cout<<"--------4 uzuotis-------"<<endl;
+    float a = 13;
+    float b = 5;
+    float c = 17.5;
+
+    //arba float a, b, c;
+    // a = 13;
+    // b = 5;
+    // c = 17.5;
+
+    cout << a + b - c << endl;
+    cout << 15 / 2 + c << endl;
+    cout << a / static_cast<double>(b) + 2 * c << endl;
+    cout << 14 % 3 + 6.3 + b / a << endl;
+    cout << static_cast<int>(c) % 5 + a - b << endl;
+    cout << 13.5 / 2 + 4.0 * 3.5 + 18 << endl;
+
+    cout<<"--------5 uzuotis-------"<<endl;
+
+    float d,e,f,g,h;
+
+    cout << "iveskite 5 skaicius: " <<endl;
+    cin >> d >> e >> f >> g >> h ;
+
+    float vidurkis = (d+e+f+g+h) / 5;
+    //isvedami 2 skaiciai po kablelio
+    cout << fixed << setprecision(2);
+    cout << "bendras vidurkis: " << vidurkis << endl;
 }
